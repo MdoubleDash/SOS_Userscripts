38ef006c1213813454b1bf91ace79a2cca66617e
M-- <masoud.m.soltani@gmail.com>
2023-04-22 15:32:40 -0400

    Update Remove RM Flair.user.js
    Indentation; Update user script description, etc.

--- Remove RM Flair.user.js
+++ Remove RM Flair.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name RemoveRM_Flair
-// @namespace RM_Flair
-// @match *://*.stackoverflow.com/questions/*
-// @version 1
+// @namespace https://github.com/MdoubleDash
+// @version 1.0
+// @description Removes the recognized member text from RM's flair
 // @author MDoubleDash
-// @description try to take over the world!
+// @match *://*.stackoverflow.com/questions/*
+// @downloadURL https://github.com/MdoubleDash/Remove_RM_Flair/edit/main/Remove RM Flair.user.js
+// @updateURL   https://github.com/MdoubleDash/Remove_RM_Flair/edit/main/Remove RM Flair.user.js
 // @grant none
 // ==/UserScript==
 
 (function() {
-'use strict';
+    'use strict';
 
-var elements = document.getElementsByClassName("s-user-card--type affiliate-badge px8 pb8 mtn4 fs-caption");
-
-while (elements.length > 0) elements[0].remove();
+    var elements = document.getElementsByClassName("s-user-card--type affiliate-badge px8 pb8 mtn4 fs-caption");
+    while (elements.length > 0) elements[0].remove();
+  
 })();
