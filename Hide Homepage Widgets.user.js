7959f7dd8c92eaaa7af11a13046184324a4dbee0
M-- <masoud.m.soltani@gmail.com>
2024-10-31 12:52:37 -0400

    Update Hide Homepage Widgets.user.js
    See https://stackapps.com/a/10635

--- Hide Homepage Widgets.user.js
+++ Hide Homepage Widgets.user.js
@@ -1,19 +1,38 @@
 // ==UserScript==
 // @name Hide Homepage Widgets
 // @namespace https://github.com/MdoubleDash
-// @version 1.0
+// @version 2.0
 // @description Removes the new widgets on Stack Overflow's homepage
-// @author MDoubleDash (@M--)
-// @contributor  @VLAZ
+// @author @canon, MDoubleDash (@M--)
+// @contributor @VLAZ
 // @match https://stackoverflow.com/
 // @downloadURL https://github.com/MdoubleDash/SOS_Userscripts/raw/main/Hide%20Homepage%20Widgets.user.js
 // @updateURL https://github.com/MdoubleDash/SOS_Userscripts/raw/main/Hide%20Homepage%20Widgets.user.js
 // @grant none
 // ==/UserScript==
 
+// (https://meta.stackoverflow.com/a/432018)
 (function() {
-    'use strict';
+  const widgetsContainer = document.getElementById("widgets-container");
+  const blogWidget = document.querySelector('.s-sidebarwidget:has(a[href^="https://stackoverflow.blog/"]');
+  const welcomeMessage = document.querySelector('.d-flex.g8');
+  const postsMessage = document.querySelector('.d-flex.fd-column.mb16');
 
-    document.documentElement.insertAdjacentHTML('beforeend', '<style type="text/css" id="hide-new-homepage-elements"> .d-flex.flex__fl-shrink0.flex__center.g24.fw-wrap.overflow-hidden { display: none !important; }</style><style type="text/css" id="hide-new-homepage-elements"> .d-flex.fd-column.mb16 { display: none !important; }</style>')
+  if (!(widgetsContainer)) return;
+  const [tagModal,tagWidget] = widgetsContainer.querySelectorAll(".widgets>:nth-child(n+4)");
+  const [tagHeader,tagContent] = tagWidget.children;
 
+  blogWidget.before(tagModal, tagWidget);
+  widgetsContainer.remove();
+
+  $('.d-flex.fd-column.mb16').find(".fs-title.fw-bold").text("Suggested posts for you");
+  postsMessage.remove();
+  welcomeMessage.before(postsMessage);
+  welcomeMessage.remove();
+
+
+  // fix display by borrowing class salad from adjacent sidebar widget...
+  tagWidget.className = "s-sidebarwidget s-anchors s-anchors__grayscale mb16";
+  tagHeader.className = "d-flex fc-black-600 pt16 pl16 fs-body2 fw-bold";
+  tagContent.className = "s-sidebarwidget--item d-flex px16";
 })();
