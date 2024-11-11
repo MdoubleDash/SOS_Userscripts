6a08d49235b81336e263b43995aca031c48ff49d
M-- <masoud.m.soltani@gmail.com>
2024-11-11 16:49:13 -0500

    Giving up on including tag widget for now
    Recent changes broke the userscript and I couldn't find an easy fix; for now, this removes all the widgets including the watched tag widget.

--- Hide Homepage Widgets.user.js
+++ Hide Homepage Widgets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Hide Homepage Widgets
 // @namespace https://github.com/MdoubleDash
-// @version 2.1
+// @version 2.1.1
 // @description Removes the new widgets on Stack Overflow's homepage
 // @author @canon, MDoubleDash (@M--)
 // @contributor @VLAZ
@@ -19,10 +19,11 @@
   const postsMessage = document.querySelector('.d-flex.fd-column.mb16');
 
   if (!(widgetsContainer)) return;
-  const [tagWidget,tagModal] = widgetsContainer.querySelector(".right-widget").children;
-  const [tagHeader,tagContent] = tagWidget.children;
+  //const widgetsContainerItems = widgetsContainer.querySelectorAll(".s-card.grid--item.p16.d-flex.fd-column.g12");
+  //const [tagWidget,tagModal] = widgetsContainerItems[2].children;
+  //const [tagHeader,tagContent] = tagWidget.children;
 
-  blogWidget.before(tagModal, tagWidget);
+  //blogWidget.before(tagWidget, tagModal);
   widgetsContainer.remove();
 
   $('.d-flex.fd-column.mb16').find(".fs-title.fw-bold").text("Suggested posts for you");
@@ -32,7 +33,9 @@
 
 
   // fix display by borrowing class salad from adjacent sidebar widget...
-  tagWidget.className = "s-sidebarwidget s-anchors s-anchors__grayscale mb16";
-  tagHeader.className = "d-flex fc-black-600 pt16 pl16 fs-body2 fw-bold";
-  tagContent.className = "s-sidebarwidget--item d-flex px16";
+  // tagWidget.className = "s-sidebarwidget s-anchors s-anchors__grayscale mb16 p16";
+  // tagHeader.className = "d-flex fc-black-600 pt16 pl16 fs-body2 fw-bold";
+  // tagContent.className = "s-sidebarwidget--item d-flex px16";
+ 
+
 })();
