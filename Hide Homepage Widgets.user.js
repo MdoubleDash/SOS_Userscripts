fbb4d51de365e1a4bc25af873909761e4713f5f8
M-- <masoud.m.soltani@gmail.com>
2025-01-30 18:31:33 -0500

    Watched tags in the right sidebar
    Adding back Watched tags to the right sidebar. N.B. the gear icon/button does not work to watch/ignore more tags.

--- Hide Homepage Widgets.user.js
+++ Hide Homepage Widgets.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name Hide Homepage Widgets
 // @namespace https://github.com/MdoubleDash
-// @version 2.1.1
+// @version 2.2
 // @description Removes the new widgets on Stack Overflow's homepage
-// @author @canon, MDoubleDash (@M--)
+// @author MDoubleDash (@M--), @canon
 // @contributor @VLAZ
 // @match https://stackoverflow.com/
 // @downloadURL https://github.com/MdoubleDash/SOS_Userscripts/raw/main/Hide%20Homepage%20Widgets.user.js
@@ -13,29 +13,33 @@
 
 // (https://meta.stackoverflow.com/a/432018)
 (function() {
+  // get the widgets container and other elements
   const widgetsContainer = document.getElementById("widgets-container");
+  if (!(widgetsContainer)) return;
+
   const blogWidget = document.querySelector('.s-sidebarwidget:has(a[href^="https://stackoverflow.blog/"]');
   const welcomeMessage = document.querySelector('.d-flex.g8');
   const postsMessage = document.querySelector('.d-flex.fd-column.mb16');
 
-  if (!(widgetsContainer)) return;
-  //const widgetsContainerItems = widgetsContainer.querySelectorAll(".s-card.grid--item.p16.d-flex.fd-column.g12");
-  //const [tagWidget,tagModal] = widgetsContainerItems[2].children;
-  //const [tagHeader,tagContent] = tagWidget.children;
+  // get the items in the widgets container
+  const widgetsContainerItems = widgetsContainer.querySelectorAll(".s-card.grid--item");
+  const tagWidget = widgetsContainerItems[2];
 
-  //blogWidget.before(tagWidget, tagModal);
+  // move the watched tag widget to the right sidebar
+  blogWidget.before(tagWidget);
   widgetsContainer.remove();
 
+  // remove the welcome message and modify the "interesting posts"
   $('.d-flex.fd-column.mb16').find(".fs-title.fw-bold").text("Suggested posts for you");
   postsMessage.remove();
   welcomeMessage.before(postsMessage);
   welcomeMessage.remove();
 
-
   // fix display by borrowing class salad from adjacent sidebar widget...
-  // tagWidget.className = "s-sidebarwidget s-anchors s-anchors__grayscale mb16 p16";
-  // tagHeader.className = "d-flex fc-black-600 pt16 pl16 fs-body2 fw-bold";
-  // tagContent.className = "s-sidebarwidget--item d-flex px16";
- 
+  tagWidget.className = "s-sidebarwidget s-anchors s-anchors__grayscale mb16 p16";
+  const tagHeader = tagWidget.querySelector(".d-flex.fc-black-600.pt16.pl16.fs-body2.fw-bold");
+  const tagContent = tagWidget.querySelector(".s-sidebarwidget--item.d-flex.px16");
+  tagHeader.className = "d-flex fc-black-600 pt16 pl16 fs-body2 fw-bold";
+  tagContent.className = "s-sidebarwidget--item d-flex px16";
 
 })();
